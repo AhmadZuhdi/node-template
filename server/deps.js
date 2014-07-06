4e9253236c6715f10a4477f04bf794dd1fd6611a
AhmadZuhdi <ahmad.zuhdi.y@gmail.com>
2014-07-06 20:42:25 +0700

    add colors dependency

--- server/deps.js
+++ server/deps.js
@@ -4,5 +4,19 @@ deps.fs = require('fs');
 deps.path = require('path');
 deps.express = require('express')();
 deps.http = require('http').Server(deps.express);
+deps.color = require('colors');
+
+deps.color.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
 
 module.exports = deps;
